4ca1a92ab4b816ff05e8f47ce4adc47149e4e001
Sunita Ramnarinesingh <sramnari@gmail.com>
2017-02-28 15:17:24 -0500

    Added Article.js schema

---
+++ models/article.js
@@ -0,0 +1,30 @@
+// Require Mongoose
+var mongoose = require('mongoose');
+
+// Create a Schema Class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
